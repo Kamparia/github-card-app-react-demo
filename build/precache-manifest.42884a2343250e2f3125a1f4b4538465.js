c573e68e72ce60166141e9802464771351ff108f
kamparia <somideolaoye@gmail.com>
2020-01-18 13:25:28 +0000

    Update: Upload of screenshot image

---
+++ build/precache-manifest.42884a2343250e2f3125a1f4b4538465.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4951132ca514bf084b0dc8ddcf272231",
+    "url": "/github-card-app-react-demo/index.html"
+  },
+  {
+    "revision": "f6e50308dbdbc0bd5658",
+    "url": "/github-card-app-react-demo/static/css/main.864a9983.chunk.css"
+  },
+  {
+    "revision": "6be4a68636e0d72d6cf7",
+    "url": "/github-card-app-react-demo/static/js/2.f7989288.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github-card-app-react-demo/static/js/2.f7989288.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6e50308dbdbc0bd5658",
+    "url": "/github-card-app-react-demo/static/js/main.a55be7fe.chunk.js"
+  },
+  {
+    "revision": "0c8e2cf846459118b658",
+    "url": "/github-card-app-react-demo/static/js/runtime-main.cc686930.js"
+  }
+]);
